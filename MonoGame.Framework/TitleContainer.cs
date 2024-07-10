7fe9d2c98b5593733bc9c784211de34fd2dfc344
tomspilman <tom@sickheadgames.com>
2024-07-10 00:57:51 -0500

    Android doesn't use absolute paths anymore.

--- MonoGame.Framework/TitleContainer.cs
+++ MonoGame.Framework/TitleContainer.cs
@@ -34,10 +34,8 @@ namespace Microsoft.Xna.Framework
                 throw new ArgumentNullException("name");
 
             // We do not accept absolute paths here.
-#if !ANDROID
             if (Path.IsPathRooted(name))
                 throw new ArgumentException("Invalid filename. TitleContainer.OpenStream requires a relative path.", name);
-#endif
 
             // Normalize the file path.
             var safeName = NormalizeRelativePath(name);
